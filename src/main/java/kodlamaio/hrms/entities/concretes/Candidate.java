b1cb6656853a52487e36c259d06f9abf723739d8
Ayse ozavci <56216719+muhendisaysee@users.noreply.github.com>
2021-05-28 23:54:03 +0300

    User ve extend ettiği (Employee, Employer ve Candidate) sınıflar yazıldı

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -0,0 +1,37 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Candidate extends User{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "identity_number")
+	private String identityNumber;
+	
+	@Column(name = "birth_year")
+	private int birthYear; 
+}
